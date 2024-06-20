36ee3d7c608040a2c6523aefc8ed2092fafa8ef0
dvita-ilmar <dvita@mail.ru>
2024-06-20 15:54:22 +0500

    Three

--- API2.py
+++ API2.py
@@ -1,2 +1,4 @@
-print('Hello!')
-print('World!')
+import requests
+
+url = 'https://api.vk.com/method/users.get?user_ids=743784474&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.199 HTTP/1.1'
+response = requests.get(url)
